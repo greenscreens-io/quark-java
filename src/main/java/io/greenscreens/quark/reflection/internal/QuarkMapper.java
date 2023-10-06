cc94c616d1163f9fc0ee1fefa3707b787d0e7f78
Green Screens Ltd <greenscreens.ltd@gmail.com>
2023-10-06 14:40:00 +0200

    Code cleanup

--- src/main/java/io/greenscreens/quark/reflection/internal/QuarkMapper.java
+++ src/main/java/io/greenscreens/quark/reflection/internal/QuarkMapper.java
@@ -84,11 +84,14 @@ public enum QuarkMapper {
 	 * @throws IllegalAccessException
 	 */
 	static MethodHandle toHandle(final Method method) throws NoSuchMethodException, IllegalAccessException {
+		return lookup.unreflect(method);
+		/*
 		final Class<?> clazz = method.getDeclaringClass();
 		final Class<?> rType = method.getReturnType();
 		final Class<?>[] pType = method.getParameterTypes();
 		final MethodType mt = MethodType.methodType(rType, pType); 
 		return lookup.findVirtual(clazz, method.getName(), mt);
+		*/
 	}
 
 	/**
