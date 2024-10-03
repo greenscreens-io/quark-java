8a28001e6aae595d762cbb7c8561e496c27ac437
Green Screens Ltd. <greenscreens.ltd@gmail.com>
2024-10-03 18:39:16 +0200

    WebSocket thread improvements

---
+++ src/main/java/io/greenscreens/quark/utils/ConcurrentFuture.java
@@ -0,0 +1,72 @@
+package io.greenscreens.quark.utils;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class ConcurrentFuture<V> implements Future<V>, Comparable<ConcurrentFuture<V>> {
+
+	private final Future<V> future;
+	
+	public ConcurrentFuture(final Future<V> future) {
+		super();
+		this.future = future;
+	}
+
+	@Override
+	public boolean cancel(boolean mayInterruptIfRunning) {
+		return future.cancel(mayInterruptIfRunning);
+	}
+
+	@Override
+	public V get() throws InterruptedException, ExecutionException {
+		return future.get();
+	}
+
+	@Override
+	public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+		return future.get(timeout, unit);
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return future.isCancelled();
+	}
+
+	@Override
+	public boolean isDone() {
+		return future.isDone();
+	}
+
+	@Override
+	public int compareTo(ConcurrentFuture<V> o) {
+
+		Future<V> obj1 = this.future;
+		Future<V>  obj2 = null;
+	    
+	    if (o!=null) {
+	    	obj2 = o.future;
+	    }
+	    
+	    if (obj1 == null) {
+	    	return -1;
+	    }
+	    if (obj2 == null) {
+	    	return 1;
+	    }
+
+	    if (obj1.equals(obj2)) {
+	        return 0;
+	    }
+	    return -1;
+	}
+
+	Future<V> unwrap() {
+		return future;
+	}
+	
+	public static <V> ConcurrentFuture<V> create(final Future<V> future) {	
+		return new ConcurrentFuture<V>(future);
+	}
+}
