130b581f001c039dc0d773fa1763f4d674e452b0
Green Screens Ltd. <greenscreens.ltd@gmail.com>
2025-03-02 09:15:27 +0100

    Code cleanup and optimizations

---
+++ src/main/java/io/greenscreens/quark/ext/ExtEncrypt.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015, 2024 Green Screens Ltd.
+ */
+package io.greenscreens.quark.ext;
+
+import io.greenscreens.quark.util.QuarkUtil;
+import jakarta.enterprise.inject.Vetoed;
+
+@Vetoed
+public class ExtEncrypt {
+
+	private String d;
+	private String k;
+	private String t;
+	private int v;
+
+	public String getD() {
+		return d;
+	}
+
+	public void setD(final String d) {
+		this.d = d;
+	}
+
+	public String getK() {
+		return k;
+	}
+
+	public void setK(final String k) {
+		this.k = k;
+	}
+
+	public int getV() {
+		return v;
+	}
+
+	public void setV(final int v) {
+		this.v = v;
+	}
+
+	public String getT() {
+		return t;
+	}
+
+	public void setT(String t) {
+		this.t = t;
+	}
+
+	public boolean isValid() {
+		return !QuarkUtil.isEmpty(d) && !QuarkUtil.isEmpty(k);  
+	}
+
+	@Override
+	public String toString() {
+		return "ExtEncrypt [d=" + d + ", k=" + k + ", v=" + v + "]";
+	}
+
+}
